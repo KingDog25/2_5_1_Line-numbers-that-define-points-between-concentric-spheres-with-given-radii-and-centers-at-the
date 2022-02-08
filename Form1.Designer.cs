a21622bfff8a9f5d89fe8b92ef4d542ce21f8c51
Alexandr Roslyakov <99288384+KingDog25@users.noreply.github.com>
2022-02-09 02:37:56 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿
+namespace _23_variant_CSarp_2_5_1
+{
+    partial class ConsentricSpheres
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelMinSum = new System.Windows.Forms.Label();
+            this.buttonInput = new System.Windows.Forms.Button();
+            this.textBoxInputEl = new System.Windows.Forms.TextBox();
+            this.labelInputEl = new System.Windows.Forms.Label();
+            this.radioButtonHandle = new System.Windows.Forms.RadioButton();
+            this.radioButtonRandom = new System.Windows.Forms.RadioButton();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox_r1 = new System.Windows.Forms.TextBox();
+            this.textBox_R2 = new System.Windows.Forms.TextBox();
+            this.labelRadiusesCount = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelMinSum
+            // 
+            this.labelMinSum.AutoSize = true;
+            this.labelMinSum.Font = new System.Drawing.Font("Segoe UI", 9.163636F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelMinSum.Location = new System.Drawing.Point(13, 418);
+            this.labelMinSum.Name = "labelMinSum";
+            this.labelMinSum.Size = new System.Drawing.Size(0, 19);
+            this.labelMinSum.TabIndex = 22;
+            // 
+            // buttonInput
+            // 
+            this.buttonInput.Location = new System.Drawing.Point(598, 35);
+            this.buttonInput.Name = "buttonInput";
+            this.buttonInput.Size = new System.Drawing.Size(106, 41);
+            this.buttonInput.TabIndex = 23;
+            this.buttonInput.Text = "Рассчитать";
+            this.buttonInput.UseVisualStyleBackColor = true;
+            this.buttonInput.Click += new System.EventHandler(this.buttonInput_Click);
+            // 
+            // textBoxInputEl
+            // 
+            this.textBoxInputEl.Enabled = false;
+            this.textBoxInputEl.Location = new System.Drawing.Point(275, 9);
+            this.textBoxInputEl.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBoxInputEl.Name = "textBoxInputEl";
+            this.textBoxInputEl.Size = new System.Drawing.Size(290, 26);
+            this.textBoxInputEl.TabIndex = 19;
+            // 
+            // labelInputEl
+            // 
+            this.labelInputEl.AutoSize = true;
+            this.labelInputEl.Font = new System.Drawing.Font("Segoe UI", 9.163636F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.labelInputEl.Location = new System.Drawing.Point(-1, 9);
+            this.labelInputEl.Name = "labelInputEl";
+            this.labelInputEl.Size = new System.Drawing.Size(270, 19);
+            this.labelInputEl.TabIndex = 20;
+            this.labelInputEl.Text = "Введите(через пробел) аргументы X:";
+            // 
+            // radioButtonHandle
+            // 
+            this.radioButtonHandle.AutoSize = true;
+            this.radioButtonHandle.Location = new System.Drawing.Point(13, 44);
+            this.radioButtonHandle.Name = "radioButtonHandle";
+            this.radioButtonHandle.Size = new System.Drawing.Size(199, 23);
+            this.radioButtonHandle.TabIndex = 25;
+            this.radioButtonHandle.Text = "Вввести значения вручную";
+            this.radioButtonHandle.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonRandom
+            // 
+            this.radioButtonRandom.AutoSize = true;
+            this.radioButtonRandom.Checked = true;
+            this.radioButtonRandom.Location = new System.Drawing.Point(13, 73);
+            this.radioButtonRandom.Name = "radioButtonRandom";
+            this.radioButtonRandom.Size = new System.Drawing.Size(235, 23);
+            this.radioButtonRandom.TabIndex = 27;
+            this.radioButtonRandom.TabStop = true;
+            this.radioButtonRandom.Text = "Заполнить случайными числами";
+            this.radioButtonRandom.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(-1, 101);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 47;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(910, 419);
+            this.dataGridView1.TabIndex = 21;
+            // 
+            // textBox_r1
+            // 
+            this.textBox_r1.Location = new System.Drawing.Point(275, 68);
+            this.textBox_r1.Name = "textBox_r1";
+            this.textBox_r1.Size = new System.Drawing.Size(123, 26);
+            this.textBox_r1.TabIndex = 28;
+            this.textBox_r1.Text = "Введите радиус r";
+            this.textBox_r1.Enter += new System.EventHandler(this.textBox_r1_Enter);
+            // 
+            // textBox_R2
+            // 
+            this.textBox_R2.Location = new System.Drawing.Point(438, 68);
+            this.textBox_R2.Name = "textBox_R2";
+            this.textBox_R2.Size = new System.Drawing.Size(127, 26);
+            this.textBox_R2.TabIndex = 29;
+            this.textBox_R2.Text = "Введите радиус R";
+            this.textBox_R2.Enter += new System.EventHandler(this.textBox_R2_Enter);
+            // 
+            // labelRadiusesCount
+            // 
+            this.labelRadiusesCount.AutoSize = true;
+            this.labelRadiusesCount.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.labelRadiusesCount.Location = new System.Drawing.Point(0, 563);
+            this.labelRadiusesCount.Name = "labelRadiusesCount";
+            this.labelRadiusesCount.Size = new System.Drawing.Size(0, 19);
+            this.labelRadiusesCount.TabIndex = 30;
+            // 
+            // ConsentricSpheres
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(889, 582);
+            this.Controls.Add(this.labelRadiusesCount);
+            this.Controls.Add(this.textBox_R2);
+            this.Controls.Add(this.textBox_r1);
+            this.Controls.Add(this.radioButtonRandom);
+            this.Controls.Add(this.radioButtonHandle);
+            this.Controls.Add(this.buttonInput);
+            this.Controls.Add(this.labelMinSum);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.labelInputEl);
+            this.Controls.Add(this.textBoxInputEl);
+            this.Name = "ConsentricSpheres";
+            this.Text = "\"Программа для определения, находится ли точка между двумя концентрическими сфера" +
+    "ми";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label labelMinSum;
+        private System.Windows.Forms.Button buttonInput;
+        private System.Windows.Forms.TextBox textBoxInputEl;
+        private System.Windows.Forms.Label labelInputEl;
+        private System.Windows.Forms.RadioButton radioButtonHandle;
+        private System.Windows.Forms.RadioButton radioButtonRandom;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox_r1;
+        private System.Windows.Forms.TextBox textBox_R2;
+        private System.Windows.Forms.Label labelRadiusesCount;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,93 @@
+﻿/*Условие: Дана матрица из 3 столбцов и 10 строк. Элементы каждой строки представляют координаты одной из 10 точек трехмерного пространства.
+Отпечатать номера строк, которыми заданы точки, находящимися между концентрическими сферами с заданными радиусами и центрами в начале координат*/
+
+/*Condition: Given a matrix of 3 columns and 10 rows. The elements of each row represent the coordinates of one of the 10 points in three-dimensional space.
+Print line numbers that define points between concentric spheres with given radii and centers at the origin*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _23_variant_CSarp_2_5_1
+{
+    public partial class ConsentricSpheres : Form
+    {
+        public ConsentricSpheres()
+        {
+            InitializeComponent();
+        }
+            private void labelInputEl_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void radioButtonHandle_CheckedChanged(object sender, EventArgs e)
+        {
+            textBoxInputEl.Enabled = radioButtonHandle.Checked;                             //сделать текстбокс активным
+        }
+
+        private void radioButtonRandom_CheckedChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox_r1_Enter(object sender, EventArgs e)
+        {
+            textBox_r1.Text = null;
+        }
+
+        private void textBox_R2_Enter(object sender, EventArgs e)
+        {
+            textBox_R2.Text = null;
+        }
+
+        private void buttonInput_Click(object sender, EventArgs e)
+        {
+            dataGridView1.RowCount = 10;                             //кол-во строк
+            dataGridView1.ColumnCount = 3;                          //столбцов
+            dataGridView1.Rows[0].HeaderCell.Value = "Кооординаты X,Y,Z";
+            dataGridView1.Columns[0].HeaderText = "X";
+            dataGridView1.Columns[1].HeaderText = "Y";
+            dataGridView1.Columns[2].HeaderText = "Z";
+            int rowsCount = dataGridView1.RowCount;
+            int columnCount = dataGridView1.ColumnCount = 3;
+            int[,] tableXYZ = new int[rowsCount, columnCount];
+            Random rnd = new Random();
+            short countPoints = 0;
+            float summSquare = 0;                                                              //для подсчета левой части каждой строки X^2 + Y^2 +Z^2 = R^2
+            int r = int.Parse(textBox_r1.Text);
+            int R = int.Parse(textBox_R2.Text);
+            labelRadiusesCount.Text = "Радиусы r="+ r +". R2=" + R + ".";
+            for (int i = 0; i < rowsCount; i++)
+            {
+                for (int j = 0; j < columnCount; j++)                                         //заполняем значения X
+                {
+                    tableXYZ[i, j] = rnd.Next(-50, 50);                                      //заполняем массив случайными значениями координат
+                    dataGridView1.Rows[i].Cells[j].Value = tableXYZ[i, j];
+                    summSquare += tableXYZ[i, j] * tableXYZ[i, j];
+                    if (summSquare < R * R && summSquare > r * r)
+                    {                                                                        //Если сумма квадратов координаты точки меньше, чем R и больше чем r
+                        countPoints++;
+                        dataGridView1.Rows[i].DefaultCellStyle.BackColor = Color.Green;
+                    }
+                    summSquare = 0;
+                }
+            }
+        }
+    }
+}
